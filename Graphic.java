17850aa22a101bf2dd3d83c6d2caecc4424da317
Snehal05-code <snehal.dhawale24@pcu.edu.in>
2025-01-31 12:42:24 +0530

    Add files via upload

---
+++ Graphic.java
@@ -0,0 +1,33 @@
+package Practice;
+
+abstract class Shape {
+    protected int x1, y1;
+    protected int x2, y2;
+
+    public void setStart(int x, int y) {
+        x1 = x;
+        y1 = y;
+    }
+
+    public void setEnd(int x, int y) {
+        x2 = x;
+        y2 = y;
+    }
+
+    public abstract void draw();
+}
+
+class Line extends Shape {
+    @Override
+    public void draw() {
+        System.out.printf("draw line from (%d,%d) to (%d,%d)%n", x1, y1, x2, y2);
+    }
+}
+
+class Rectangle extends Shape {
+    @Override
+    public void draw() {
+        System.out.printf("draw rectangle from (%d,%d) to (%d,%d)%n", x1, y1, x2, y2);
+    }
+}
+
